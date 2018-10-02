6d9e9a2f075e0017e53bd77692a597884c830505
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2018-10-02 13:41:14 +0530

    Readme added

---
+++ README.md
@@ -0,0 +1,11 @@
+# SDC task 1 - TODO in ANGULAR.js
+
+This is a todo list made using angular.js as a frontend framework.
+
+## Features
+
+- **Editable tasks** :- You can edit a task by clicking on it s it converts into a input field.
+- **Add Task** :- Add tasks by writing your task in new task field and click on create button
+- **Delete Task** :- Delete tasks by clicking on delete button provided along side each of the task.
+- **Mark task as completed** :- Mark task as completed and then they will be striked with a line.
+- **Integration with Firebase** :- The todo list with all the changes is saved to firebase database so you can open the list again and have all your old changes intact.
--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 var app = angular.module("todoAngularSDC", []);
 
-app.controller("mainCtrl", function(mainService) {
+app.controller("mainCtrl", function() {
   var vm = this;
+  this.currStatusShow = 1;
   this.newTask = "";
-  this.tasks = { "make a todo list": "incomplete", dance: "complete" };
-  this.taskName = Object.keys(this.tasks);
-  this.alert = function() {
-    alert("HI");
-  };
+  this.tasks = [
+    { title: "make a todo list", status: false },
+    { title: "dance", status: true }
+  ];
   this.createTask = function() {
-    console.log("hi");
-    vm.tasks[vm.newTask] = "incomplete";
-    vm.taskName = Object.keys(vm.tasks);
+    // console.log("hi");
+    vm.tasks.push({ title: vm.newTask, status: false });
+    // console.log(vm.tasks);
     vm.newTask = "";
   };
   this.deleteTask = function(task) {
-    delete vm.tasks[task];
-    vm.taskName.splice(vm.taskName.indexOf(task), 1);
+    vm.tasks.splice(vm.tasks.indexOf(task), 1);
   };
-  mainService.getPosts().then(res => {
-    vm.posts = res.data;
-  });
+  //   mainService.getPosts().then(res => {
+  //     vm.posts = res.data;
+  //   });
+  //   $scope.$watch(
+  //     "tasks",
+  //     function(a, b) {
+  //       console.log(a);
+  //       console.log(b);
+  //       console.log("WOOAHH");
+  //     },
+  //     true
+  //   );
 });
 
-app.filter("plural", function() {
-  return function(input) {
-    return input + "s";
-  };
-});
+// app.filter("plural", function() {
+//   return function(input) {
+//     return input + "s";
+//   };
+// });
 
-app.service("mainService", function($http) {
-  this.getPosts = () => {
-    return $http.get("https://jsonplaceholder.typicode.com/posts");
-  };
-});
+// app.service("mainService", function($http) {
+//   this.getPosts = () => {
+//     return $http.get("https://jsonplaceholder.typicode.com/posts");
+//   };
+// });
